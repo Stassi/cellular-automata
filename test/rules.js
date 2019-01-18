638cad3026e1c09753003388103d3f66d0b5a4ac
Andreas Stassivik <andreas@stassi.net>
2019-01-17 17:17:32 -0800

    test common rules

--- test/rules.js
+++ test/rules.js
@@ -1,8 +1,34 @@
 import { expect } from 'chai';
-import rules from '../src';
+import rule from '../src';
 
-describe('rules', () => {
-  it('should return the debug object', () => {
-    expect(rules()).to.include({ rules: true });
+describe('rule', () => {
+  describe('30', () => {
+    it('should include the rules', () => {
+      expect(rule(30)).to.include({
+        '000': 0,
+        '001': 1,
+        '010': 1,
+        '011': 1,
+        '100': 1,
+        '101': 0,
+        '110': 0,
+        '111': 0,
+      });
+    });
+  });
+
+  describe('110', () => {
+    it('should include the rules', () => {
+      expect(rule(110)).to.include({
+        '000': 0,
+        '001': 1,
+        '010': 1,
+        '011': 1,
+        '100': 0,
+        '101': 1,
+        '110': 1,
+        '111': 0,
+      });
+    });
   });
 });
