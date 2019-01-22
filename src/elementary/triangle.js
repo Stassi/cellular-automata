5e1058393ecd6a0dfda9424379ee4e7701eb900d
Andreas Stassivik <andreas@stassi.net>
2019-01-21 19:30:01 -0800

    +toElementaryRule

--- src/elementary/triangle.js
+++ src/elementary/triangle.js
@@ -1,8 +1,18 @@
-// TODO: Implement
-const triangle = () => ([
-  [0],
-  [1, 1, 1],
-  [1, 1, 0, 0, 1],
-]);
+import { pipe } from 'ramda';
+import elementaryRule from './rule';
+
+const toElementaryRule = ({ rule, ...props }) => ({
+  ...props,
+  elementaryRule: elementaryRule(rule),
+});
+
+const triangle = pipe(
+  toElementaryRule,
+  () => ([
+    [0],
+    [1, 1, 1],
+    [1, 1, 0, 0, 1],
+  ]),
+);
 
 export default triangle;
