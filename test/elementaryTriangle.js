cde7692fea6e25a9d71ba06597289116c4414993
Andreas Stassivik <andreas@stassi.net>
2019-01-22 06:06:34 -0800

    test rule 110

--- test/elementaryTriangle.js
+++ test/elementaryTriangle.js
@@ -2,11 +2,13 @@ import { expect } from 'chai';
 import { elementaryTriangle as triangle } from '../src';
 
 describe('elementary triangle', () => {
-  describe('rule: 30', () => {
-    describe('height: 5', () => {
+  describe('height: 5', () => {
+    const height = 5;
+
+    describe('rule: 30', () => {
       it('should exhibit rule 30 behavior', () => {
         expect(triangle({
-          height: 5,
+          height,
           outerState: 0,
           rule: 30,
           seed: [1],
@@ -16,6 +18,23 @@ describe('elementary triangle', () => {
           [1, 1, 0, 0, 1],
           [1, 1, 0, 1, 1, 1, 1],
           [1, 1, 0, 0, 1, 0, 0, 0, 1],
+        ]);
+      });
+    });
+
+    describe('rule: 110', () => {
+      it('should exhibit rule 110 behavior', () => {
+        expect(triangle({
+          height,
+          outerState: 0,
+          rule: 110,
+          seed: [1],
+        })).to.have.deep.ordered.members([
+          [1],
+          [1, 1, 0],
+          [1, 1, 1, 0, 0],
+          [1, 1, 0, 1, 0, 0, 0],
+          [1, 1, 1, 1, 1, 0, 0, 0, 0],
         ]);
       });
     });
