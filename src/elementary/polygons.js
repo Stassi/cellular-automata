bcb41627407e70c7b2f9462cddcb4b5718bd351e
Andreas Stassivik <andreas@stassi.net>
2019-02-10 02:31:00 -0800

    set default rectangle width

--- src/elementary/polygons.js
+++ src/elementary/polygons.js
@@ -1,8 +1,13 @@
 import polygon from './polygon';
 
-const rectangle = ({ ...props }) => polygon({
+const rectangle = ({
+  height,
+  width,
+  ...props
+}) => polygon({
   ...props,
   vertices: 4,
+  width: width || height,
 });
 
 const triangle = ({ startRow = [1], ...props }) => polygon({
