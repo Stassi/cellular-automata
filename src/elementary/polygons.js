efe43fece4d8d5b50d74777c8790fbe8e006d6ae
Andreas Stassivik <andreas@stassi.net>
2019-02-10 01:39:19 -0800

    -default seed props

--- src/elementary/polygons.js
+++ src/elementary/polygons.js
@@ -1,20 +1,16 @@
 import polygon from './polygon';
 
 const rectangle = ({
-  seed = [1, 0, 1],
   ...props,
 }) => polygon({
   ...props,
-  seed,
   vertices: 4,
 });
 
 const triangle = ({
-  seed = [1],
   ...props,
 }) => polygon({
   ...props,
-  seed,
   vertices: 3,
 });
 
