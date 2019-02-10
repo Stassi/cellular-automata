17c293ac0b65ba623bffa1f12d26d3c27a9df027
Andreas Stassivik <andreas@stassi.net>
2019-02-10 02:38:39 -0800

    Revert "set default rectangle width"
    This reverts commit bcb41627

--- src/elementary/polygons.js
+++ src/elementary/polygons.js
@@ -1,13 +1,8 @@
 import polygon from './polygon';
 
-const rectangle = ({
-  height,
-  width,
-  ...props
-}) => polygon({
+const rectangle = ({ ...props }) => polygon({
   ...props,
   vertices: 4,
-  width: width || height,
 });
 
 const triangle = ({ startRow = [1], ...props }) => polygon({
